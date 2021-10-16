3623d8e76bde835166e4ccd2fae45ebbc300bf5d
Pablo Dominguez Villacanas <pablo.dominguez@situm.com>
2021-10-16 17:03:03 +0200

    Able to fetch and persist on Atlas with SOC

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from .types import restresponse_from_dict, restresponse_to_dict
-from .nosql.service import find_last
+from .nosql.service import find_last, persist
 from .spider.service import fetch
 
-# fetch()
-print(len(find_last().lista_eess_precio))
+def fetch_and_persist():
+    print('persist and log')
+    persist(fetch())
+
+fetch_and_persist()
 
 __name__ == '__main__'
--- src/nosql/service.py
+++ src/nosql/service.py
@@ -1,13 +1,16 @@
 from .db import raw_collection
-from ..types import RestResponse, restresponse_from_dict
+
+from ..types import RestResponse, restresponse_from_dict, restresponse_to_dict
 
 
 def find_by_date():
     # TODO
     return None
 
+
 def find_last() -> RestResponse:
     return restresponse_from_dict(raw_collection.find_one())
 
+
 def persist(data: RestResponse):
-    raw_collection.pe
+    raw_collection.insert_one(restresponse_to_dict(data))
