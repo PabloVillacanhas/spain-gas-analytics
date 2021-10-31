831b7829f5f58b952e5f8c9306fd7468273f249e
Pablo Dominguez Villacanas <pablo.dominguez@situm.com>
2021-10-31 10:32:53 +0100

    Serviing react buildt code as static file

---
+++ src/backend/resources.py
@@ -0,0 +1,25 @@
+from flask import Blueprint
+from flask_restful import Api, Resource
+from .schemas import GasStationSchema
+from backend.sql.service import get
+
+gasstations_v1_bp = Blueprint('gasstations_v1_0_bp', __name__)
+api = Api(gasstations_v1_bp)
+gas_station_schema = GasStationSchema()
+
+
+class GasStationResource(Resource):
+    def get(self, gasstation_id):
+        gs = get(gasstation_id)
+        result = gas_station_schema.dump(gs)
+        return result
+
+
+# class GasStationPricesResource(Resource):
+#     def get(self, id):
+#         films = get(id)
+#         return result
+
+
+api.add_resource(GasStationResource, '/api/v1/gasstations/<int:gasstation_id>', endpoint='gasstations')
+# api.add_resource(FilmResource, '/api/v1/gasstations/<int:gasstation_id>', endpoint='film_resource')
