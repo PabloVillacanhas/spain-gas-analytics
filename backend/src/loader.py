a5c123d720bb6d0d707e68e8954c430e61af7ac3
Pablo Dominguez Villacanas <pablo.dominguez@situm.com>
2021-12-11 22:27:49 +0100

    Compose

--- backend/src/loader.py
+++ backend/src/loader.py
@@ -14,7 +14,8 @@ def fetch_and_persist():
     logging.debug('Try to fetch and persist')
     last_mongo_response = find_last()
     if transform_to_utc_datetime(last_mongo_response.fecha).date() != datetime.utcnow().date() or True:
-        logging.info("Last response registered in mongo differs from today UTC")
+        logging.info(
+            "Last response registered in mongo differs from today UTC")
         logging.info("Fetching remote data given by government")
         rest_response = fetch()
         logging.info("Fetching remote data given by government")
@@ -26,9 +27,8 @@ def fetch_and_persist():
 if __name__ == '__main__':
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_BIND'] = "engine"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/gas'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@postgres:5432/gas'
     db.init_app(app)
     initConfiguration()
     with app.app_context():
         fetch_and_persist()
-
