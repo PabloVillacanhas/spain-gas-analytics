87d3a7969d00c129c294fd537f6973b9f3f25a2d
Pablo Villacanas <Pablo.Villacanas@shell.com>
2022-05-24 14:13:41 +0200

    Set dinamically the port of container to CI/CD

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,7 +4,10 @@ b="app"
 cd src
 
 if [ "$1" = "app" ]; then 
-	gunicorn --bind 0.0.0.0:5001 wsgi
+	if [[ -z "${PORT}" ]]; then 
+		gunicorn --bind 0.0.0.0:$PORT wsgi
+	else
+		gunicorn --bind 0.0.0.0:5001 wsgi
 else
 	python3 loader.py
 fi
