e96fa6c0671528842b35725a5fd2e9e0607a3a86
Pablo Villacanas <Pablo.Villacanas@shell.com>
2022-05-24 16:42:17 +0200

    Echoing port

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,9 +6,9 @@ cd src
 echo $PORT
 if [ "$1" = "app" ]; then 
 	if [[ -z "${PORT}" ]]; then 
-		gunicorn --bind 0.0.0.0:$PORT wsgi
-	else
 		gunicorn --bind 0.0.0.0:5001 wsgi
+	else
+		gunicorn --bind 0.0.0.0:$PORT wsgi
 	fi
 else
 	python3 loader.py
